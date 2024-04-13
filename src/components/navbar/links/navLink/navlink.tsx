98af9339a94797d569d997173d5182914f23e550
dontyellwillcry <papaporo@proton.me>
2024-04-13 14:17:09 -0500

    Added the <Link> element to navlink that conditionally reders the Styles.container if the pathName === item.title && if styles.active is true

--- src/components/navbar/links/navLink/navlink.tsx
+++ src/components/navbar/links/navLink/navlink.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "./navLink.module.css";
 import Link from "next/link";
 
@@ -11,13 +13,18 @@ interface NavLinkProps {
 
 // item is a prop from Links.tsx. Its sending the link that was being mapped over
 const NavLink: React.FC<NavLinkProps> = ({ item }) => {
-  return (
-    //The href attribute of the Link is set to the path property from the item
+  const pathName: string = usePathname();
 
+  return (
+    //?The href attribute of the Link is set to the path property from the item
     /*The className attribute is set to ${styles.container}, 
     which applies a CSS class (defined in a separate CSS module) */
-
-    <Link href={item.path} className={`${styles.container}`}>
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathName === item.title && styles.active
+      }`}
+    >
       {item.title}
     </Link>
   );
