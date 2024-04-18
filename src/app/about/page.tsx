febc54667c6e398aaeaee952c2960a7e27649879
dontyellwillcry <papaporo@proton.me>
2024-04-18 16:45:32 -0500

    added some more styling to the headers, paragraphs and images in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,28 +7,32 @@ const AboutPage = () => {
     //! If you are using a parent div the container position should be position: relative;
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1>About Let's Cook Together</h1>
-        <h1>Save all of those new recipes and try some new ones!!!</h1>
-        <p>
+        <h2 className={styles.subtitle}>About Let's Cook Together</h2>
+        <h1 className={styles.title}>Save all of those new recipes and try some new ones!!!</h1>
+        <p className={styles.desc}>
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum."
         </p>
-        <div className={styles.quoteNum}>
-          <h1>10 K+</h1>
-          <h1>2344 K+</h1>
-          <h1>5 K+</h1>
-        </div>
-        <div className={styles.quotes}>
-          <p>Dicks I've Sucked</p>
-          <p>Cunts Smashed</p>
-          <p>Cat's pet</p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Dicks Sucked</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Dicks Sucked</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Dicks Sucked</p>
+          </div>
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image src="/images/icons/crockpot.png" alt="Crockpot" fill />
+        <Image className={styles.img} src="/images/icons/crockpot.png" alt="Crockpot" fill />
       </div>
     </div>
   );
