dfb656d709b24716fe3605aa3bff14c0c4f90a8f
dontyellwillcry <papaporo@proton.me>
2024-05-02 00:24:14 -0500

    added a fetch to get a pokemon api

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,32 @@
-const AdminPage = () => {
-    return (
-        <div>AdminPage</div>
-    )
-}
+
+
+const AdminPage: React.FC = () => {
+  
+  
+    const fetchPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+            .then((response) => {
+                // Check if the response is ok
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                // Parse the JSON response
+                return response;
+            })
+            .then((data) => {
+                // Log the parsed JSON data
+                console.log(data);
+            })
+            .catch((error) => {
+                // Log any errors that occur during the fetch
+                console.error('Error:', error);
+            });
+    };
+    
+
+    fetchPokemon();
+
+  return <div>Pokemon</div>;
+};
 
 export default AdminPage;
