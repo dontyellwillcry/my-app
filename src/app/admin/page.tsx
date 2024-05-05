4128463b3a5eed6a6d746a03d5cdb27223017896
dontyellwillcry <papaporo@proton.me>
2024-05-05 00:16:20 -0500

    added another fetch alternative

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,32 +1,66 @@
-
+"use client";
+import { useState, useEffect } from "react";
 
 const AdminPage: React.FC = () => {
-  
-  
-    const fetchPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
-            .then((response) => {
-                // Check if the response is ok
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                // Parse the JSON response
-                return response;
-            })
-            .then((data) => {
-                // Log the parsed JSON data
-                console.log(data);
-            })
-            .catch((error) => {
-                // Log any errors that occur during the fetch
-                console.error('Error:', error);
-            });
-    };
-    
+  const [pokemon, setPokemon] = useState<string>("");
+  const [otherPokemon, setOtherPokemon] = useState<string>("");
 
-    fetchPokemon();
+  async function fetchPokemon() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu/");
 
-  return <div>Pokemon</div>;
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setOtherPokemon(data.name);
+      console.log("Here is ", data.name);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  async function fetchMoves() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/diglett/");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setPokemon(data.name);
+      console.log("Here is ", data.name);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error getting Pokemon");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("This is the .then/.catch API call",data.name);
+    })
+    .catch((error) => {
+      console.error("my error is", error);
+    });
+
+  useEffect(() => {
+      fetchPokemon()
+      fetchMoves()
+  }, []);
+
+  return (
+    <div>
+      <p>This is from the .try/catch API call {pokemon}</p>
+      <p>This is from the .try/catch API call {otherPokemon}</p>
+    </div>
+  );
 };
 
 export default AdminPage;
