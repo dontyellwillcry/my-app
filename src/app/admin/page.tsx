86262d42f8115b4f974c28da6e830f51d7e200e5
dontyellwillcry <papaporo@proton.me>
2025-06-13 17:50:41 -0500

    removed fluff from admin page. Not sure what I'll be adding there.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,40 +3,18 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const AdminPage: React.FC = () => {
-  const [pokemon, setPokemon] = useState<string | null>("");
-  const [ability, setAbility] = useState<string | null>("");
+ 
 
   // Example of a then/catch request client side
-  const fetchPokemon = () => {
-    axios
-      .get("/api/pokemon")
-      .then((res) => {
-        setPokemon(res.data.name);
-      })
-      .catch((error) => {
-        console.error("There was a problem fetching the name:", error);
-      });
-  };
-
-  // Example of a try/catch request client side
-  // const fetchAbility = async () => {
-  //   try {
-  //     const res = await axios.get("/api/ability");
-  //     setAbility(res.data.name);
-  //   } catch (error) {
-  //     console.error("There was a problem fetching the ability:", error);
-  //   }
-  // };
+  
 
   useEffect(() => {
-    fetchPokemon();
-    // fetchAbility();
+    
   }, []);
 
   return (
     <div>
-      <p>This is from the .try/catch API call {pokemon}</p>
-      <p>This is from the .try/catch API call {ability}</p>
+      Admin Page
     </div>
   );
 };
